75849ff94c69ccc57715bc36bba664e635197498
Yoav Hirshberg <yoav.yh@gmail.com>
2022-10-13 21:18:11 +0300

    consuming ControllPanel in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { GameHeader } from './components/game-header/GameHeader';
 import { Board } from './components/board/Board';
+import { ControllPanel } from './components/controll-panel/ControllPanel';
 
 function App() {
   const selectedBoardSize = 4; //TODO: set dynamically by the player
   return (
     <>
     <GameHeader/>
+    <ControllPanel/>
     <Board boardSize={selectedBoardSize}/>
     </>
   );
