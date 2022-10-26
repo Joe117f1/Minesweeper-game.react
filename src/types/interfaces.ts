5134d365ea702c2d07832f3ff871ff1cf48941a2
Yoav Hirshberg <yoav.yh@gmail.com>
2022-10-26 17:24:49 +0300

    moving interfaces to a designated files, version1

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,7 +1,29 @@
-export interface ICell {
+export enum EActions {
+  MINE = 'mine',
+  FLAGGED = 'flagged',
+  CLICKED = 'clicked',
+} //TODO: move to enums
+
+export interface CellState {
+  id?: string;
   xCoord: number;
   yCoord: number;
   isMine: boolean;
   isFlagged: boolean;
   isClicked: boolean;
+  value: any; //TODO: fix any type, fix duplication with ICell
+}
+
+export interface Cell {
+  id?: string;
+  xCoord: number;
+  yCoord: number;
+  isMine: boolean;
+  isFlagged: boolean;
+  isClicked: boolean;
+  value: any;
+  cellReducer?: (
+    state: CellState,
+    action: { type: EActions; id: string }
+  ) => CellState;
 }
