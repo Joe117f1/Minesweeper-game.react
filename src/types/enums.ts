00fd83de905ea33291911305374fe6014be06ab8
Yoav Hirshberg <yoav.yh@gmail.com>
2022-11-04 11:23:46 +0200

    needed emojies, for now just as consts

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -4,3 +4,18 @@ export enum EIcons {
   EXPLOSION = '#',
   EXPLODED = '@',
 }
+//needed emojies
+const BEFORE_GAME_EMOJI = '&#128512';
+const IN_GAME_EMOJI = '&#128517';
+const SKULL_EMOJI = '&#128128';
+const WINNING_EMOJI = '&#129312';
+const EXPLODED = '&#128165';
+const MINE = '&#128261';
+const FLAG = '&#128681';
+const FALSE_FLAG = '&#10060';
+const HINT = '&#128161';
+const USED_HINT = '&#128162';
+const LIVES = '&#128151';
+const NO_LIVES = '&#128148';
+const COUNT_MINES = 'COUNT-MINES';
+const REVEAL_NEIGHBORS = 'REVEAL-NEIGHBORS';
